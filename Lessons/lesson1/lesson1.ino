99a24a6176fce8c5f8d5ae490937c22fe76d16a4
Marc Sibert <marc@sibert.fr>
2022-01-22 00:05:54 +0100

    Update lesson1.ino

--- Lessons/lesson1/lesson1.ino
+++ Lessons/lesson1/lesson1.ino
@@ -1,9 +1,22 @@
-void setup()
-{ // put your setup code here, to run once:
+/**
+ * Copyright (c) 2022 by Marc SIBERT
+ * 
+ * @see ../LICENCE file
+ */
+
+/**
+ * Called once.
+ */
+void setup() {
+  // put your setup code here, to run once:
   pinMode(7, OUTPUT);
 }
-void loop()
-{ // put your main code here, to run
-  repeatedly:
-  digitalWrite(7, HIGH);
+
+/**
+ * Called repeatedly.
+ */
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(7, LOW);
+  // digitalWrite(7, HIGH);
 }
