92a380db22e87e87355055e4e1dd5a0099c3e71b
Marc Sibert <marc@sibert.fr>
2022-01-24 19:05:31 +0100

    Update lesson3.ino

--- Lessons/lesson3/lesson3.ino
+++ Lessons/lesson3/lesson3.ino
@@ -1,19 +1,21 @@
+/**
+ * Copyright (c) 2022, Marc SIBERT
+ * 
+ * @see ../LICENSE
+ */
 
-int LED = 3;
-int brightness = 0;
-int fadeAmount = 5;
+const auto LED = 3;   // Pin 3
+auto brightness = 0;
+auto fadeAmount = 5;
 
-void setup()
-{
-  pinMode(3, OUTPUT);
+void setup() {
+  pinMode(LED, OUTPUT);
 }
 
-void loop()
-{
-  analogWrite(LED, brightness);
-  brightness = brightness + fadeAmount;
-  if (brightness <= 0 || brightness >= 255)
-  {
+void loop() {
+  analogWrite(LED, brightness);               // analogic value, not binarie as HIGH/LOW
+  brightness += fadeAmount;                   // brightness = brightness + fadeAmount
+  if (brightness <= 0 || brightness >= 255) {
     fadeAmount = -fadeAmount;
   }
   delay(30);
