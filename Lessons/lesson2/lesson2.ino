2e5dfd3998b105a1034b442c6c13d9d439c6f525
Marc Sibert <marc@sibert.fr>
2022-01-24 19:05:23 +0100

    Update lesson2.ino

--- Lessons/lesson2/lesson2.ino
+++ Lessons/lesson2/lesson2.ino
@@ -1,11 +1,23 @@
-void setup()
-{
+/**
+ * Copyright (c) 2022, Marc SIBERT
+ * 
+ * @see ../LICENSE
+ */
+
+void setup() {
+  // Set pin mode to output
   pinMode(7, OUTPUT);
 }
 
-void loop()
-{
+/**
+ * Set pin on & off every 2 seconds.
+ */
+void loop() {
+  // Set pin on (+5v)
   digitalWrite(7, HIGH);
   delay(1000);
+
+  // Set pin off (0v)
   digitalWrite(7, LOW);
+  delay(1000);
 }
