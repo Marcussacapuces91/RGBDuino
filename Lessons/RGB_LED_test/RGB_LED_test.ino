f600af76e698fb347959456fbd10bc58e135be00
Marc Sibert <marc@sibert.fr>
2022-08-29 15:16:34 +0200

    Update RGB_LED_test.ino

--- Lessons/RGB_LED_test/RGB_LED_test.ino
+++ Lessons/RGB_LED_test/RGB_LED_test.ino
@@ -1,21 +1,31 @@
-#include <Adafruit_NeoPixel.h>
-#ifdef __AVR__
-  #include <avr/power.h>
-#endif
+/**
+ * Copyright (c) 2022, Marc SIBERT
+ * 
+ * @see ../LICENSE
+ */
 
-#define PIN1 6
-#define PIN2 12
- int LED1=8;
- int LED2=9;
- int LED3=10;
- int LED4=11;
- int LED7=2;
- int LED8=3;
- int LED9=4;
- int LED10=5;
- int LED11=6;
- int LED12=7;
- int n;
+#include <Adafruit_NeoPixel.h>
+
+enum 
+{
+  RGB_1 = 12,
+  RGB_2 = 13,
+
+  TONEPIN = 8,
+
+  LED_1 = 2,
+  LED_2,
+  LED_3,
+  LED_4,
+  LED_5,
+  LED_6,
+  LED_7,
+  LED_8,
+  LED_9,
+  LED_10,
+
+};
+
 #define NTD0 -1
 #define NTD1 294
 #define NTD2 330
@@ -47,8 +57,7 @@
 #define EIGHTH 0.25
 #define SIXTEENTH 0.625
 
-int tune[]=
-{
+const int tune[] = {
   NTD3,NTD3,NTD4,NTD5,
   NTD5,NTD4,NTD3,NTD2,
   NTD1,NTD1,NTD2,NTD3,
@@ -66,8 +75,8 @@ int tune[]=
   NTD1,NTD1,NTD2,NTD3,
   NTD2,NTD1,NTD1
 };
-float durt[]=
-{
+
+const float durt[] = {
   1,1,1,1,
   1,1,1,1,
   1,1,1,1,
@@ -85,8 +94,9 @@ float durt[]=
   1,1,1,1,
   1+0.5,0.5,1+1,
 };
-int length;
-int tonepin=6;
+
+static const auto length = sizeof(tune)/sizeof(tune[0]);
+
 // Parameter 1 = number of pixels in strip1
 // Parameter 2 = Arduino pin number (most are valid)
 // Parameter 3 = pixel type flags, add together as needed:
@@ -95,108 +105,96 @@ int tonepin=6;
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 //   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
-Adafruit_NeoPixel strip1 = Adafruit_NeoPixel(1, PIN1, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel strip2 = Adafruit_NeoPixel(1, PIN2, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip1 = Adafruit_NeoPixel(1, RGB_1, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip2 = Adafruit_NeoPixel(1, RGB_2, NEO_GRB + NEO_KHZ800);
 // IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
 // pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
 // and minimize distance between Arduino and first pixel.  Avoid connecting
 // on a live circuit...if you must, connect GND first.
 
 void setup() {
-  // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
-  #if defined (__AVR_ATtiny85__)
-    if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
-  #endif
-  // End of trinket special code
-for(n=2;n<=13;n++)
-{
-  pinMode(n, OUTPUT);  
-}
+  for(unsigned n = LED_1 ; n <= LED_10 ; ++n) {
+    digitalWrite(n, LOW);
+    pinMode(n, OUTPUT);
+  }
 
   strip1.begin();
   strip2.begin();
   strip1.show(); // Initialize all pixels to 'off'
   strip2.show();
 
-   pinMode(tonepin,OUTPUT);
-  length=sizeof(tune)/sizeof(tune[0]);
+  pinMode(TONEPIN, OUTPUT);
 }
 
 void loop() {
   // Some example procedures showing how to display to the pixels:
-  colorWipe(strip1.Color(50, 0, 0), 100); // Red
-   colorWipe(strip2.Color(255, 0, 0), 100); // Red
-  colorWipe(strip1.Color(0, 50, 0), 100); // Green
-    colorWipe(strip2.Color(0, 255, 0), 100); // Green
-  colorWipe(strip1.Color(0, 0, 50), 100); // Blue
-    colorWipe(strip2.Color(0, 0, 255), 100); // Blue
+  colorWipe(Adafruit_NeoPixel::Color(50, 0, 0), 500);   // Red
+  colorWipe(Adafruit_NeoPixel::Color(255, 0, 0), 500);  // Red
+  colorWipe(Adafruit_NeoPixel::Color(0, 50, 0), 500);   // Green
+  colorWipe(Adafruit_NeoPixel::Color(0, 255, 0), 500);  // Green
+  colorWipe(Adafruit_NeoPixel::Color(0, 0, 50), 500);   // Blue
+  colorWipe(Adafruit_NeoPixel::Color(0, 0, 255), 500);  // Blue
+  colorWipe(Adafruit_NeoPixel::Color(0, 0, 50), 500);   // Blue
+  colorWipe(Adafruit_NeoPixel::Color(0, 0, 0), 500);    // Dark
   turn1();
   clean();
  // turn2();
  // clean();
  // turn3();
  // clean();
-for(int x=0;x<length;x++)
-  {
-    tone(tonepin,tune[x]);
-    delay(200*durt[x]);
-    noTone(tonepin);
+  for(unsigned x = 0 ; x < length ; x++) {
+    tone(TONEPIN, tune[x]);
+    delay(190 * durt[x]);
+    noTone(TONEPIN);
+    delay(10 * durt[x]);
   }
   delay(2000);
 }
 // Fill the dots one after the other with a color
 
 
-void colorWipe(uint32_t c, uint8_t wait) {
-  for(uint16_t i=0; i<strip1.numPixels(); i++) {
+void colorWipe(uint32_t c, unsigned wait) {
+  for (unsigned i = 0; i < strip1.numPixels(); i++) {
     strip1.setPixelColor(i, c);
     strip1.show();
     delay(wait);
   }
-    for(uint16_t i=0; i<strip2.numPixels(); i++) {
+  for (unsigned i = 0; i < strip2.numPixels(); i++) {
     strip2.setPixelColor(i, c);
     strip2.show();
     delay(wait);
   }
 }
-void turn1()
-{
-  for(n=2;n<=13;n++)
-  {
-      digitalWrite(n,HIGH);
+
+void turn1() {
+  for (unsigned n = LED_1 ; n <= LED_10 ; n++) {
+      digitalWrite(n, HIGH);
       delay(300);
   }
-    for(n=2;n<=13;n++)
-  {
-      digitalWrite(n,LOW);
+  for (unsigned n = LED_1 ; n <= LED_10 ; n++) {
+      digitalWrite(n, LOW);
       delay(300);
   }
 }
 
-void turn2()
-{ 
-  for(int x=0;x<=2;x++)
-  {
-    for(n=2;n<=13;n++)
-  {
+void turn2() { 
+  for (auto x = 0 ; x <= 2 ; x++) {
+    for(auto n = 2 ; n <= 13 ; n++) {
       digitalWrite(n,HIGH);   
-  }
-  delay(300); 
-    for(n=2;n<=13;n++)
-  {
+    }
+    delay(300); 
+    for (auto n = 2 ; n <= 13 ; n++) {
       digitalWrite(n,LOW); 
-  }
-  delay(300);
+    }
+    delay(300);
   }
 }
 
-void turn3()
-{
-  for(int x=0;x<=2;x++)
-  {
+void turn3() {
+  for (auto x = 0 ; x <= 2 ; x++) {
     digitalWrite(8,HIGH);
     digitalWrite(9,HIGH);
-    for(n=10;n<=11;n++){
+    for(auto n = 10 ; n <= 11 ; n++) {
       digitalWrite(n,LOW);
     }
     delay(300);
@@ -205,16 +203,15 @@ void turn3()
     digitalWrite(8,LOW);
     digitalWrite(9,LOW);
     delay(300);
-    for(n=2;n<=11;n++){
+    for(auto n = 2 ; n <= 11 ; n++) {
       digitalWrite(n,LOW);
     }
     delay(300);
   }
 }
 
-void clean()
-{ for(n=2;n<=11;n++)
-  {
+void clean() { 
+  for(unsigned n = LED_1 ; n <= LED_10 ; n++) {
       digitalWrite(n,LOW); 
   }
   delay(300);
